839ee5186726a4e91a2f8762d53eb0ae8e7aff61
Simone Esposito <simoexpo@outlook.com>
2018-07-06 00:24:15 +0200

    Fixed last modifcation date and time for altervista ftp file

--- lib/webcamfornolo_backend/dal/altervista/altervista_dal.ex
+++ lib/webcamfornolo_backend/dal/altervista/altervista_dal.ex
@@ -1,5 +1,4 @@
 defmodule WebcamfornoloBackend.Dal.Altervista.AltervistaDal do
-  alias WebcamfornoloBackend.Dal.Altervista.Model.FileInfo
   require Logger
 
   @host 'ftp.webcamfornolo.altervista.org'
@@ -11,12 +10,10 @@ defmodule WebcamfornoloBackend.Dal.Altervista.AltervistaDal do
       {:ok, pid} = :inets.start(:ftpc, host: @host)
       :ok = :ftp.user(pid, user(), password())
       :ok = :ftp.type(pid, :binary)
-      {:ok, output} = :ftp.ls(pid)
 
       created_at =
-        ls_to_file_list(to_string(output))
-        |> Enum.find(fn %FileInfo{file_name: file_name} -> file_name == name end)
-        |> Map.get(:created_at)
+        :ftp.quote(pid, 'MDTM #{name}')
+        |> get_last_modify_date_time()
 
       local_path = "#{@save_directory}#{name}"
       :ok = :ftp.recv(pid, to_charlist(name), to_charlist(local_path))
@@ -28,14 +25,18 @@ defmodule WebcamfornoloBackend.Dal.Altervista.AltervistaDal do
     end
   end
 
-  defp ls_to_file_list(ls_output) do
-    String.split(ls_output, "\r\n")
-    |> Enum.map(fn x -> String.split(x) |> Enum.drop(5) end)
-    |> Enum.filter(fn x -> Enum.count(x) == 4 end)
-    |> Enum.map(fn [month, day, year_or_time, file_name] ->
-      FileInfo.from(day, month, year_or_time, file_name)
-    end)
-    |> Enum.filter(fn x -> x != :error end)
+  defp get_last_modify_date_time(ftp_mdtm) do
+    time_string =
+      to_string(ftp_mdtm)
+      |> String.split()
+      |> Enum.at(1)
+
+    Regex.run(~r/(\d\d\d\d)(\d\d)(\d\d)(\d\d)(\d\d)(\d\d)/, time_string)
+    |> Enum.drop(1)
+    |> Enum.join(",")
+    |> Timex.parse!("{YYYY},{M},{D},{h24},{m},{s}")
+    |> DateTime.from_naive!("Etc/UTC")
+    |> Timex.to_datetime("Europe/Rome")
   end
 
   defp user, do: to_charlist(Application.get_env(:webcamfornolo_backend, :altervista_ftp_user))
