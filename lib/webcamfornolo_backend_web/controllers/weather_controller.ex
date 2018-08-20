93cc64519be2dede713850e2fdd5025c6f3a5335
Simone Esposito <simoexpo@outlook.com>
2018-08-20 10:22:57 +0200

    Added CORS header for weather data API

--- lib/webcamfornolo_backend_web/controllers/weather_controller.ex
+++ lib/webcamfornolo_backend_web/controllers/weather_controller.ex
@@ -3,8 +3,14 @@ defmodule WebcamfornoloBackendWeb.WeatherController do
 
   def get_weather(conn, _params) do
     case WebcamfornoloBackend.get_weather_info() do
-      {:ok, data} -> conn |> put_status(200) |> json(data)
-      :error -> conn |> put_status(500) |> json(%{})
+      {:ok, data} ->
+        conn
+        |> put_status(200)
+        |> put_resp_header("Access-Control-Allow-Origin", "https://webcamfornolo.altervista.org")
+        |> json(data)
+
+      :error ->
+        conn |> put_status(500) |> json(%{})
     end
   end
 end
