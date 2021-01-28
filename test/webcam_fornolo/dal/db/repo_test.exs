4de51289886562489521cd9638ff4047d106df0b
Simone Esposito <simoexpo@outlook.com>
2021-01-28 23:26:47 +0100

    fix github action

--- test/webcam_fornolo/dal/db/repo_test.exs
+++ test/webcam_fornolo/dal/db/repo_test.exs
@@ -1,7 +1,7 @@
 defmodule WebcamFornolo.Dal.Db.RepoTest do
   use WebcamFornolo.DataCase
 
-  test "Repo should be able to get retrieve data paginated" do
+  test "Repo should be able to retrieve data paginated" do
     assert 0 == 1
   end
 end
