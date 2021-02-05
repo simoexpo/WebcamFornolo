81c47f1e5697064fb5cdab4b0198ba40b36ce3e8
Simone Esposito <simoexpo@outlook.com>
2021-02-05 01:44:59 +0100

    temp

--- lib/webcam_fornolo/dal/netatmo/netatmo_dal.ex
+++ lib/webcam_fornolo/dal/netatmo/netatmo_dal.ex
@@ -6,6 +6,16 @@ defmodule WebcamFornolo.Dal.Netatmo.NetatmoDal do
   alias WebcamFornolo.Util.DateTimeUtil
 
   @cache_key :netatmo_token
+  @cache Application.get_env(:webcam_fornolo, :app_cache)
+
+  @email Application.get_env(:elixatmo, :user_email)
+  @password Application.get_env(:elixatmo, :user_password)
+  @user_data %UserData{email: @email, password: @password}
+
+  @app_id Application.get_env(:elixatmo, :app_id)
+  @client_secret Application.get_env(:elixatmo, :client_secret)
+  @app_data %AppData{app_id: @app_id, client_secret: @client_secret}
+
   @token_scope [TokenScope.read_station()]
 
   @spec get_weather() :: :error | {:ok, map()}
@@ -19,9 +29,9 @@ defmodule WebcamFornolo.Dal.Netatmo.NetatmoDal do
 
   @spec get_access_token() :: :error | {:ok, String.t()}
   defp get_access_token() do
-    case Cachex.get(cache(), @cache_key) do
+    case Cachex.get(@cache, @cache_key) do
       {:ok, nil} ->
-        with {:ok, token} <- ElixAtmo.get_access_token(user_data(), app_data(), @token_scope),
+        with {:ok, token} <- ElixAtmo.get_access_token(@user_data, @app_data, @token_scope),
              {:ok, token} <- save(token) do
           {:ok, token.access_token}
         else
@@ -34,7 +44,7 @@ defmodule WebcamFornolo.Dal.Netatmo.NetatmoDal do
             {:ok, token.access_token}
 
           _ ->
-            with {:ok, token} <- ElixAtmo.refresh_access_token(token.refresh_token, app_data()),
+            with {:ok, token} <- ElixAtmo.refresh_access_token(token.refresh_token, @app_data),
                  {:ok, token} <- save(token) do
               {:ok, token.access_token}
             else
@@ -48,26 +58,9 @@ defmodule WebcamFornolo.Dal.Netatmo.NetatmoDal do
   defp save(token) do
     expires_at = DateTimeUtil.now() |> Timex.shift(seconds: token.expires_in)
 
-    case Cachex.put(cache(), @cache_key, {token, expires_at}) do
+    case Cachex.put(@cache, @cache_key, {token, expires_at}) do
       {:ok, _} -> {:ok, token}
       _ -> :error
     end
   end
-
-  @spec user_data :: ElixAtmo.Model.UserData.t()
-  defp user_data do
-    email = Application.get_env(:elixatmo, :user_email)
-    password = Application.get_env(:elixatmo, :user_password)
-    %UserData{email: email, password: password}
-  end
-
-  @spec app_data :: ElixAtmo.Model.AppData.t()
-  defp app_data do
-    app_id = Application.get_env(:elixatmo, :app_id)
-    client_secret = Application.get_env(:elixatmo, :client_secret)
-    %AppData{app_id: app_id, client_secret: client_secret}
-  end
-
-  @spec cache() :: atom()
-  defp cache(), do: Application.get_env(:webcam_fornolo, :app_cache)
 end
